6ce4bb93470988c8dfa45630c4a22e66e0f71eda
rebs-code <jack.rebussi@gmail.com>
2024-03-29 18:55:51 +0100

    Add player name editing functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+    console.log(isEditing);
+  }
+
+  let shownElement = <span className="player-name">{name}</span>;
+  let buttonText = "Edit";
+
+  if (isEditing) {
+    shownElement = <input type="text" required />;
+    buttonText = "Save";
+  }
+
   return (
     <li>
-      <span className="players">
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {shownElement}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{buttonText}</button>
     </li>
   );
 }
