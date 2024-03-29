fcbfe596c416347308c9ee8812767f7f2d8407d9
rebs-code <jack.rebussi@gmail.com>
2024-03-29 17:51:56 +0100

    Add header and player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="player1" symbol="X" />
+          <Player name="player2" symbol="O" />
+        </ol>
+        game boards
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
