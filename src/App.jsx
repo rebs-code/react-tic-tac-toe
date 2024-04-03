9ff33ba79ff81075e773ce78d0a536ed904a77b6
rebs-code <jack.rebussi@gmail.com>
2024-04-03 12:45:05 +0200

    Add active player functionality and highlight active player in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="player1" symbol="X" />
-          <Player initialName="player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="player2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayer={activePlayer}
+        />
       </div>
     </main>
   );
